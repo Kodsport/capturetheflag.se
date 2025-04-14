9f0828b30cdf3340ce55ba485e664f6aac52cb39
Movitz Sunar <movitz.sunar@gmail.com>
2025-04-14 05:41:34 +0200

    stuff

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -21,13 +21,12 @@ export const teamsData: TeamItem[] = [
     name: "RoyalRoppers",
     location: { lat: 59.329, lng: 18.068 },
     city: "Stockholm",
-    country: "Sverige",
+    country: "Sweden",
     website: "https://royalroppers.team",
     description: "A top-performing CTF team from KTH Royal Institute of Technology",
     achievements: [
-      "1:a Undutmaning 2025, 2024, 2023",
-      "1:a Crate-CTF 2024, 2022",
-      "NorseCode member",
+      "1st Undutmaningen 2025, 2024, 2023",
+      "1st Crate-CTF 2024, 2022",
     ]
   },
   {
@@ -35,11 +34,34 @@ export const teamsData: TeamItem[] = [
     name: "Swedish National Hacking Team",
     city: "",
     location: { lat: 59.11, lng: 15.45 },
-    country: "Sverige",
+    country: "Sweden",
     website: "https://snht.se",
     description: "Svenska hackinglandslaget tävlar i ECSC",
     achievements: [
-      "6:a European Cybersecurity Championship 2022",
+      "6th European Cybersecurity Championship 2022",
+    ]
+  },
+  {
+    id: 3,
+    name: "ChaGu",
+    city: "Gothemburg",
+    country: "Sweden",
+    location: { lat: 57.708, lng: 11.974 },
+    website: "https://chagu.org/",
+    description: "A Chalmers + Göteborgs Universitet team",
+    achievements: [
+      "???"
+    ]
+  },
+  {
+    id: 4,
+    name: "KebabEngineers",
+    city: "Stockholm",
+    country: "Sweden",
+    location: { lat: 57.708, lng: 11.974 },
+    description: "Engieers that love kebab (and CTF)",
+    achievements: [
+      "???"
     ]
   }
 ];
